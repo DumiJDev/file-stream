2680ad98f037fcb386c1d95ae268d8903797a463
Dumilde Fernando <dumildesfernando@outlook.com>
2023-11-17 12:19:56 +0100

    feat: add control over status when writing

--- src/main/java/io/github/dumijdev/filestream/model/WriterFileStreamImpl.java
+++ src/main/java/io/github/dumijdev/filestream/model/WriterFileStreamImpl.java
@@ -8,29 +8,67 @@ import java.util.function.Function;
 
 public final class WriterFileStreamImpl implements WriterFileStream<Object> {
   private Thread executor;
+  private boolean isDone = false;
 
   @Override
-  public void write(DataStream<Object> dataStream, File file, Function<Object, String> transformer) {
+  public void write(DataStream<Object> dataStream, File file, Function<Object, String> transformer) throws IOException {
+    dataStream.begin();
+
+    if (!file.exists()) {
+      file.createNewFile();
+    }
 
     Runnable task = () -> {
       try (BufferedWriter bw = new BufferedWriter(new FileWriter(file))) {
-        dataStream.begin();
         while (dataStream.hasNext()) {
+          var next = dataStream.next();
           if (transformer != null) {
-            bw.append(transformer.apply(dataStream.next()));
+            bw.append(transformer.apply(next));
           } else {
-            bw.append(dataStream.next().toString());
+            bw.append(next.toString());
           }
           bw.newLine();
         }
+        bw.flush();
       } catch (InterruptedException | IOException e) {
         throw new RuntimeException(e);
+      } finally {
+        isDone = true;
       }
     };
 
     executor = new Thread(task);
 
     executor.start();
+  }
+
+  @Override
+  public void writeAndWait(DataStream<Object> dataStream, File file, Function<Object, String> transformer) throws IOException {
+    dataStream.begin();
+
+    if (!file.exists()) {
+      file.createNewFile();
+    }
+
+    try (BufferedWriter bw = new BufferedWriter(new FileWriter(file))) {
+      while (dataStream.hasNext()) {
+        var next = dataStream.next();
+        if (transformer != null) {
+          bw.append(transformer.apply(next));
+        } else {
+          bw.append(next.toString());
+        }
+        bw.newLine();
+      }
+      bw.flush();
+    } catch (InterruptedException | IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  @Override
+  public boolean isDone() {
+    return isDone;
   }
 
   @Override
--- src/test/java/io/github/dumijdev/tests/WriterFileStreamTests.java
+++ src/test/java/io/github/dumijdev/tests/WriterFileStreamTests.java
@@ -13,6 +13,7 @@ import org.junit.jupiter.api.Test;
 import java.io.File;
 import java.nio.file.Files;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class WriterFileStreamTests {
   private WriterFileStream<Object> writerFileStream;
@@ -20,35 +21,105 @@ public class WriterFileStreamTests {
   private File fileOut;
   private File fileIn;
 
-  @SneakyThrows
   @BeforeEach
-  void loadFile() {
-    fileOut = Files.createTempFile("test_out", ".txt").toFile();
-    fileIn = new File(Objects.requireNonNull(ReaderFileStreamTests.class.getResource("/test.txt")).toURI());
+  void setup() {
     writerFileStream = new WriterFileStreamImpl();
     readerFileStream = new ReaderFileStreamImpl();
+  }
+
+  @AfterEach
+  void cleanup() {
+    if (fileOut != null && fileOut.exists()) {
+      fileOut.delete();
+    }
   }
 
   @SneakyThrows
   @Test
   void shouldWriteFile() {
-    var stream = readerFileStream.read(fileIn);
-    writerFileStream.write(stream, fileOut);
+    // Arrange
+    fileIn = new File(Objects.requireNonNull(ReaderFileStreamTests.class.getResource("/test.txt")).toURI());
+    fileOut = new File("test_out4.txt");
 
-    Assertions.assertTrue(fileOut.exists());
+    // Act
+    var writeStream = readerFileStream.read(fileIn);
 
+    writerFileStream.write(writeStream, fileOut);
+
+    while (!writerFileStream.isDone()) {
+      System.out.println("Waiting...");
+    }
+
+    // Assert
+    Assertions.assertTrue(fileOut.exists(), "The output file should exist");
+    Assertions.assertTrue(Files.readAllBytes(fileOut.toPath()).length > 0, "Content should match");
   }
 
   @SneakyThrows
   @Test
   void shouldWriteFileWithTransform() {
-    var stream = readerFileStream.read(fileIn);
-    writerFileStream.write(stream, fileOut, a -> a.toString().toUpperCase());
+    // Arrange
+    fileIn = new File(Objects.requireNonNull(ReaderFileStreamTests.class.getResource("/test.txt")).toURI());
+    fileOut = new File("upper_case.txt");
 
-    Assertions.assertTrue(fileOut.exists());
+    // Act
+    var writeStream = readerFileStream.read(fileIn);
 
+    writerFileStream.write(writeStream, fileOut, o -> o.toString().toUpperCase());
+
+    while (!writerFileStream.isDone()) {
+      System.out.println("Waiting...");
+    }
+
+    var readStream = readerFileStream.read(fileOut);
+
+    // Assert
+    Assertions.assertTrue(fileOut.exists(), "The output file should exist");
+    Assertions.assertEquals(writeStream.getAll().stream().map(o -> o.toString().toUpperCase()).collect(Collectors.toList()),
+        readStream.getAll(),
+        "Content should match");
   }
 
+  @SneakyThrows
+  @Test
+  void shouldWriteAndReadFile() {
+    // Arrange
+    fileIn = new File(Objects.requireNonNull(ReaderFileStreamTests.class.getResource("/test.txt")).toURI());
+    fileOut = new File("test_out2.txt");
 
+    // Act
+    var writeStream = readerFileStream.read(fileIn);
+
+    writerFileStream.writeAndWait(writeStream, fileOut);
+
+    var readStream = readerFileStream.read(fileOut);
+
+    // Assert
+    Assertions.assertTrue(fileOut.exists(), "The output file should exist");
+    Assertions.assertEquals(writeStream.getAll().size(), readStream.getAll().size(), "Content should match");
+  }
+
+  @SneakyThrows
+  @Test
+  void shouldWriteAsyncAndReadFile() {
+    // Arrange
+    fileIn = new File(Objects.requireNonNull(ReaderFileStreamTests.class.getResource("/test.txt")).toURI());
+    fileOut = new File("test_out1.txt");
+
+    // Act
+    var writeStream = readerFileStream.read(fileIn);
+
+    writerFileStream.write(writeStream, fileOut);
+
+    while (!writerFileStream.isDone()) {
+      System.out.println("Waiting...");
+    }
+
+    var readStream = readerFileStream.read(fileOut);
+
+    // Assert
+    Assertions.assertTrue(fileOut.exists(), "The output file should exist");
+    Assertions.assertEquals(writeStream.getAll().size(), readStream.getAll().size(), "Content should match");
+  }
 
 }
